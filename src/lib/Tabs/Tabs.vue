e7d34866bcff2a6615b7d66d7d51e9daaf3c9d0f
yinlu <yinlu1228@163.com>
2021-07-13 22:53:28 +0800

    tabs

--- src/lib/Tabs/Tabs.vue
+++ src/lib/Tabs/Tabs.vue
@@ -1,9 +1,26 @@
 <template>
-  <div></div>
+  <div class="lu-tabs">
+    <div class="lu-tabs-nav" ref="container">
+      <div class="lu-tabs-nav-item"></div>
+      <div class="lu-tabs-nav-indicator" ref="indicator"></div>
+    </div>
+    <div class="lu-tabs-content">
+      <component :is="current" />
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-export default {}
+export default {
+  name: 'lu-tabs',
+  props: {
+    selected: {
+      type: String,
+      default: ''
+    }
+  },
+  setup(props, context) {}
+}
 </script>
 
 <style lang="scss" scoped>
